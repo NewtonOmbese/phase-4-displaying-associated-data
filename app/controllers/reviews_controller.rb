6f208f553e1a888078234f1cbddd578e34f26bdc
Newton Ombese <newtonombese1@gmail.com>
2022-10-01 09:31:56 +0300

    Discover the include keyword

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    render json: reviews, include: :controller
   end
 
 end
